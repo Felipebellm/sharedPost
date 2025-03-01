7684917c4251c0537cf813be1201f4c7bb1d355d
Felipe Bell Marques <felipe.bell@smc.priv>
2025-03-01 12:34:56 -0300

    validation for login and register

--- php/Shared_Posts/app/controllers/Users.php
+++ php/Shared_Posts/app/controllers/Users.php
@@ -1,5 +1,5 @@
 <?php 
-    class Users {
+    class Users extends Controller{
         public function __construct() {
 
         }
@@ -8,9 +8,127 @@
             // Check for POST
             if($_SERVER['REQUEST_METHOD'] == 'POST') {
                 // Process form 
+
+                //Sanitaze POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+                // Init data
+                $data =[
+                    'name' => trim($_POST['name']),
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'confirm_password' => trim($_POST['confirm_password']),
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => '',
+                ];
+
+                // Vlaidate Email
+                if (empty($data['email'])){
+                    $data['email_err'] = "Please enter email";
+                }
+
+                // Validate Name
+                if (empty($data['name'])){
+                    $data['name_err'] = "Please enter Name";
+                }
+
+                // Validate Password
+                if (empty($data['password'])){
+                    $data['password_err'] = "Please enter Password";
+                } elseif (strlen($data['password']) < 6 ) {
+                    $data['password_err'] = "Password must be atleast 6 characters";
+                }
+
+                // Validate Confirm Password
+                if (empty($data['confirm_password'])) {
+                    $data['confirm_password_err'] = "Please Confirm Password";
+                } else {
+                    if ($data['password'] != $data['confirm_password']) {
+                        $data['confirm_password_err'] = "Password do not match";
+                    }
+                }
+
+                // Make Sure errors are empty
+                if (empty($data['email_err']) &&
+                    empty($data['name_err']) &&
+                    empty($data['password_err']) &&
+                    empty($data['confirm_password_err'])) {
+                        die('SUCCESS');
+                } else {
+                    // Load View with errors
+                    $this->view('users/register', $data);
+                }
+
+
             } else {
-                // Load form
-                echo 'load form';
+                // Init data
+                $data =[
+                    'name' => '',
+                    'email' => '',
+                    'password' => '',
+                    'confirm_password' => '',
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => '',
+                ];
+                // Load view
+                $this->view('users/register', $data);
+            }
+        }
+
+
+        public function login() {
+            // Check for POST
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Process form 
+
+
+                //Sanitaze POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+
+                 // Init data
+                 $data =[
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'email_err' => '',
+                    'password_err' => '',
+                ];
+
+                // Validate email
+                if (empty($data['email'])){
+                    $data['email_err'] = "Please enter email";
+                }
+
+                // Validate Password
+                if (empty($data['password'])){
+                    $data['password_err'] = "Please enter Password";
+                } elseif (strlen($data['password']) < 6 ) {
+                    $data['password_err'] = "Password must be atleast 6 characters";
+                }
+
+                // Make Sure errors are empty
+                if (empty($data['email_err']) &&
+                    empty($data['password_err'])) {
+                        die('SUCCESS');
+                } else {
+                    // Load View with errors
+                    $this->view('users/login', $data);
+                }
+
+            } else {
+                // Init data
+                $data =[
+                    'email' => '',
+                    'password' => '',
+                    'email_err' => '',
+                    'password_err' => ''
+                ];
+                // Load view
+                $this->view('users/login', $data);
             }
         }
     }
--- php/Shared_Posts/app/views/inc/navbar.php
+++ php/Shared_Posts/app/views/inc/navbar.php
@@ -1,23 +1,23 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
-  <div class="container-fluid">
+  <div class="container">
     <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse w-100" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
+          <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT . '/pages/about'; ?>">About</a>
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT . '/users/register'; ?>">Register</a>
         </li>
-        <li class="nav-item active">
+        <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT . '/users/login'; ?>">Login</a>
         </li>
       </ul>
