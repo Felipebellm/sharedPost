5aeb330919e97ceb08968e2d655596ae341f4c86
Felipe Bell Marques <felipe.bell@smc.priv>
2025-04-14 13:34:38 -0300

    users complete

--- php/Shared_Posts/app/models/User.php
+++ php/Shared_Posts/app/models/User.php
@@ -22,6 +22,23 @@
             }
         }
 
+        // Login User
+        public function login($email, $password) {
+            $this->db->query('SELECT * FROM users WHERE email = :email');
+            $this->db->bind(':email', $email);
+
+            $row = $this->db->single();
+
+            $hashed_password = $row->password;
+
+            if(password_verify($password, $hashed_password)){
+                return $row;
+            } else {
+                return false;
+            }
+
+        }
+
         // Find User by email
         public function findUserByEmail($email) {
             //find user by email
@@ -33,7 +50,6 @@
             $row = $this->db->single();
 
             //Check row
-
             if ($this->db->rowCount() > 0) {
                 return true;
             } else {
