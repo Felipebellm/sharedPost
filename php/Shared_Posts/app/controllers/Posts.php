fe6e16d9553508c0868cf65ab048b7f09af874ef
Felipe Bell Marques <felipe.bell@smc.priv>
2025-04-15 13:18:27 -0300

    Post listing

--- php/Shared_Posts/app/controllers/Posts.php
+++ php/Shared_Posts/app/controllers/Posts.php
@@ -1,7 +1,19 @@
 <?php
     class Posts extends Controller {
+        public function __construct(){
+            if(!isLoggedIn()) {
+                redirect('users/login');
+            }
+
+            $this->postModel = $this->model('Post');
+        }
         public function index() {
-            $data = [];
+            //GET posts
+            $posts = $this->postModel->getPosts();
+
+            $data = [
+                'posts' => $posts
+            ];
 
             $this->view('posts/index', $data);
         }
